c019c3b4daf5e9d17a24e714ea6075c203bc5fdd
batoolalali <runner@fv-az18.i2rao5an0nxunp5fyo3wek3orc.cx.internal.cloudapp.net>
2020-07-06 07:13:20 +0000

    Updates

---
+++ precache-manifest.d0aa1d719c4a7b4fb91816ff74f86d91.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "90af1dfe6a2ee3b8022eb60731223aeb",
+    "url": "/todo/index.html"
+  },
+  {
+    "revision": "63202a5c89462106b442",
+    "url": "/todo/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "56a604cf01c50b6bb894",
+    "url": "/todo/static/css/main.b06360a0.chunk.css"
+  },
+  {
+    "revision": "63202a5c89462106b442",
+    "url": "/todo/static/js/2.f35ed575.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/todo/static/js/2.f35ed575.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56a604cf01c50b6bb894",
+    "url": "/todo/static/js/main.5be97657.chunk.js"
+  },
+  {
+    "revision": "4b7c96a078d3292937b1",
+    "url": "/todo/static/js/runtime-main.d4ce9cd5.js"
+  }
+]);
